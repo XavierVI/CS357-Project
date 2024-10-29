9ee1ee492425f9a268888f3300eac5b73bbe3933
xavier <vigenesisvi@gmail.com>
2024-10-29 10:15:23 -0600

    added animation example and updated README

--- Main.hs
+++ Main.hs
@@ -1,2 +1,31 @@
 import Graphics.Gloss
-main = display (InWindow "Nice Window" (200, 200) (10, 10)) white (Circle 80)
+import Graphics.Gloss.Interface.IO.Animate (animateIO, Controller (controllerSetRedraw))
+
+{-  -}
+
+window :: Display
+window = InWindow "Nice Window" (200, 200) (10, 10)
+
+box :: Float -> Float -> Path
+box x y = [(u, w) | u <- [0..x], w <- [0..y]]
+
+{- This can only form a diagonal path -}
+linePath :: Float -> Float -> Path
+linePath x y = [(u, w) | (u, w) <- zip [0..x] [0..y]]
+
+
+{- 
+Notes:
+- the display function takes a display, color, and picture as input and returns an IO. It opens a new window and displays the given picture.
+
+- Displays describe how Gloss should display its output (what the window should look like)
+- 
+
+ -}
+main = display window white (Line (linePath 80 80))
+
+-- animating a growing circle
+animationFunc :: Float -> Picture
+animationFunc time = Circle (2*time)
+
+mainAnimatedCircle = animate window white animationFunc
--- README.md
+++ README.md
@@ -8,3 +8,7 @@ cabal update
 cabal install --lib gloss
 ```
 
+## Sources
+1. [Graphics.Gloss Documentation](https://hackage.haskell.org/package/gloss-1.13.2.2/docs/Graphics-Gloss.html)
+2. [Gloss Documentation](https://hackage.haskell.org/package/gloss)
+3. [Wikipedia: Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life)
