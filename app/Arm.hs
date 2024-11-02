c6d78280fb09a430d1b690f77fc11c79c8847830
xavier <vigenesisvi@gmail.com>
2024-11-02 11:46:41 -0600

    cleaned up code and added a better model for the robot arm

--- app/Arm.hs
+++ app/Arm.hs
@@ -2,48 +2,44 @@ module Arm where
 
 
 import Graphics.Gloss
-import Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss.Geometry.Angle
 import Math
 
 {- 
 
   This file is going to be where we define the model of the arm.
   Notes:
-  - the idea right now is the arm is going to be our model that has its own state (links and joint angles).
-  - the links are defined as a [[Point]] and each end point is considered a joint (besides the very first [Point]).
-  - What we want to do now, is write a function that can draw the arm in the desired orientation to the window. This is going to be a sequence of Lines, so we need to use Pictures to return a single Picture.
+  - Our model (RobotArm) is defined as a list of Links, and each Link has a property length and angle.
+  The angle is going to be relative to the previous link, or the x-axis for the first link.
 
  -}
 
 -- Model of the arm (Links, joints)
-data Arm = Arm [([Point], Float)]
-
+-- data Arm = Arm [([Point], Float)]
+data Link = Link Float Float
+data RobotArm = RobotArm [Link]
 
 -- Initial state functions
 armLength :: Float
 armLength = 100
 
-initialState :: [([Point], Float)]
-initialState = [
-  ([(0,   0), (200, 0)],    90),
-  ([(200, 0), (400, 0)],     0)
-  ]
-
-initialJointAngles :: [Float]
-initialJointAngles = [90, -30]  -- Initial angles in degrees
+initialState :: RobotArm
+initialState = RobotArm [Link 100 90, Link 45 45]
 
 
--- Functions to draw a link and joint
-{-
-  (x, y) = starting point
-  length = length of the line segment
+{- 
+ - This function takes a starting point and returns a Line that represents the link.
 
-  This function only draws a simple line.
--}
-drawLink :: ([Point], Float) -> Picture
-drawLink ([(x0, y0), (x1, y1)], angle) = Line [(x0, y0), rotateLine angle (x1, y1)]
+ -}
+drawLink :: Link -> Point
+drawLink (Link linkLength angle) = endPoint
+  where
+    x = linkLength * cos (degToRad angle)
+    y = linkLength * sin (degToRad angle)
+    endPoint = (x, y)
 
-displayArm :: Picture
-displayArm = Pictures (map drawLink initialState)
+displayArm :: RobotArm -> Picture
+displayArm (RobotArm links) = Color red $ Line ((0, 0) : [(x, y) | (x, y) <- points])
+  where
+    points = map drawLink links
 
-   
--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,6 @@ import Code
 import Arm
 
 main :: IO()
-main = display window white displayArm
+main = display window white (displayArm (RobotArm [Link 100 45, Link 45 90]))
   
     
