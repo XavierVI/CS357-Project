0e4b35b4ccd0820f7dfaf0f4c900b35a13e927d8
xavier <vigenesisvi@gmail.com>
2024-11-03 11:57:41 -0700

    cleaned up code/comments and removed Code.hs

--- app/Arm.hs
+++ app/Arm.hs
@@ -6,29 +6,23 @@ import Graphics.Gloss.Geometry.Angle
 import Math
 
 {- 
-
-  This file is going to be where we define the model of the arm.
-  Notes:
-  - Our model (RobotArm) is defined as a list of Links, and each Link has a property length and angle.
-  The angle is going to be relative to the previous link, or the x-axis for the first link.
+  This file is going to be where we define the model of the arm and functions to draw/move the arm in the window.
 
  -}
 
 -- Model of the arm (Links, joints)
--- data Arm = Arm [([Point], Float)]
 data Link = Link Float Float
 data RobotArm = RobotArm [Link]
 
--- Initial state functions
-armLength :: Float
-armLength = 100
-
-initialState :: RobotArm
-initialState = RobotArm [Link 100 90, Link 45 45]
-
 
 {- 
- - This function takes a starting point and returns a Line that represents the link.
+  - These functions are used to display the arm in the window
+
+  - drawLink takes a Link and a starting point and returns the end point for the link
+  
+  - generatePoints takes a list of links and a point which represents the base of the arm. It generates a list of points which represent the location of each joint
+  
+  - displayArm takes a RobotArm and returns a Line, which is a visual representation of the arm.
 
  -}
 drawLink :: Link -> Point -> Point
@@ -38,14 +32,17 @@ drawLink (Link linkLength angle) (x, y) = endPoint
     y' = linkLength * sin (degToRad angle)
     endPoint = (x+x', y+y')
 
-displayArm :: RobotArm -> Picture
-displayArm (RobotArm links) = Line ((0, 0) : [(x, y) | (x, y) <- points])
-  where
-    points = generatePoints links (0,0)
 
 generatePoints :: [Link] -> Point -> [Point]
 generatePoints [] _ = []
 generatePoints (link:links) prevPoint = endPoint : generatePoints links endPoint
   where
     endPoint = drawLink link prevPoint
+
+
+displayArm :: RobotArm -> Picture
+displayArm (RobotArm links) = Line ((0, 0) : [(x, y) | (x, y) <- points])
+  where
+    points = generatePoints links (0,0)
+
 
