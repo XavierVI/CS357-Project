b93ce32afd51c69c6ef436a237c3fba801c2a4c9
xavier <vigenesisvi@gmail.com>
2024-11-11 14:05:00 -0700

    modified Main.hs to display both the arm and box

--- app/Main.hs
+++ app/Main.hs
@@ -3,65 +3,31 @@ import Arm ( RobotArm(RobotArm), Link(Link), drawArm, updateArm, updateArmGD )
 import Box ( Box(Box), drawBox, constructBox, updateBoxPosition )
 import Graphics.Gloss.Data.ViewPort ()
 
+data Sim = Sim RobotArm Box
+
 window :: Display
 window = InWindow "Window" (800, 800) (50, 50)
 
+arm :: RobotArm
+arm = RobotArm [Link 100 90, Link 100 (-90)]
+
+box :: Box
+box = constructBox 30 50
+
+
+
+drawSim :: RobotArm -> Box -> Picture
+drawSim arm box = Pictures [drawArm arm, drawBox box]
+
+updateSim :: ViewPort -> Float -> Sim -> Sim
+updateSim _ dt (Sim RobotArm links Box size points mass) = undefined
+
 main :: IO()
--- main = display window white (drawBox (constructBox 50 50))
 main = simulate
   window
   white
   60
-  (constructBox 100 50)
-  drawBox
-  updateBoxPosition
+  (Sim arm box)
+  (drawSim arm box)
+  updateSim
 
--- main = simulate
---   window
---   white
---   60
---   (RobotArm [Link 100 90, Link 100 (-90)])
---   drawArm
---   updateArm
-
-
-{- 
-  Examples
- 
-box :: Float -> Float -> Path
-box x y = [(u, w) | u <- [0..x], w <- [0..y]]
-
-{- This can only form a diagonal path -}
-linePath :: Float -> Float -> Path
-linePath x y = [(u, w) | (u, w) <- zip [0..x] [0..y]]
-
-
-
--- animating a growing circle
-animationFunc :: Float -> Picture
-animationFunc time = Circle (2*time)
--- mainAnimatedCircle = animate window white animationFunc
-
--- pendulum example: https://mmhaskell.com/blog/2019/3/25/making-a-glossy-game-part-1
-type Model = (Float, Float)
-
-simulationRate :: Int
-simulationRate = 20
-
-initialModel :: Model
-initialModel = (0,0)
-
-drawingFunc :: Model -> Picture
-drawingFunc (theta, dtheta) = Line [(0, 0), (200 * cos theta, 200 * sin theta)]
-
-updateFunc :: ViewPort -> Float -> Model -> Model
-updateFunc _ dt (theta, dtheta) = (theta + dt * dtheta, dtheta - dt * (cos theta))
-    
-pendulumMain = simulate
-  window
-  white
-  simulationRate
-  initialModel
-  drawingFunc
-  updateFunc
-  -}
