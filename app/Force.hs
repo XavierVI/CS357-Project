7c207cf4c56b02128d6f19f3d6bc6032abc8b583
Jay Megill <jmegill7@unm.edu>
2024-11-18 16:04:03 -0700

    Removed some over complications, and remembered Point exists

--- app/Force.hs
+++ app/Force.hs
@@ -2,31 +2,25 @@ module Force where
 
 import Graphics.Gloss.Data.Picture
 
-data Position = Position {px :: Float, py :: Float}
-    deriving (Show)
+type Velocity = (Float, Float)
 
-data Velocity = Velocity {vx :: Float, vy :: Float}
-    deriving (Show)
-
-data Model = Model {getVelocity :: Velocity, getPostition :: Position, getObject :: Object, getMass :: Float}
-    deriving (Show)
+data Model = Model {getVelocity :: Velocity, getPostition :: Point, getObject :: Object, getMass :: Float}
 
 -- Sum type to hold the object in question currently just a box
 data Object = Box Float Float
-    deriving (Show)
+
 
 -- Convert object withn the Model into a picture currently just the Box but more rendering could be possible
 objectToPicture :: Object -> Picture
 objectToPicture (Box w l) = Polygon [(w,l),(-w,l),(-w,-l),(w,-l)]
 
 initialVelocity :: Velocity
-initialVelocity = Velocity 0 0
+initialVelocity = (0,0)
 
-initialPosition :: Position
-initialPosition = Position 0 0
+
 
 initialBox :: Model
-initialBox = Model initialVelocity initialPosition (Box 25 25) 50
+initialBox = Model initialVelocity (0,0) (Box 25 25) 50
 
 -- Currently doesn't work because haskell doesn't like [Model]
 --State :: [Model]
@@ -40,8 +34,8 @@ modelToPicture = toPicture
     where 
         toPicture model = 
             let
-                x = px $ getPostition model
-                y = py $ getPostition model
+                x = fst $ getPostition model
+                y = snd $ getPostition model
                 o = getObject model
             in Translate x y $ objectToPicture o
 
@@ -52,12 +46,12 @@ modelToModel = next
     where
         next model = 
             let 
-            v = getVelocity model
-            p = getPostition model
-            nv = v {vx = vx v, vy = vy v - g}
-            np = p {px = px p + vx v, py = py p + vy v}
-            g = 1
-            in Model {getVelocity = nv, getPostition = np, getObject = getObject model, getMass = getMass model } 
-            --                                               shouldn't have to do this, but it crashed without it
+            (vx,vy) = getVelocity model
+            (px,py) = getPostition model
+            (nvx,nvy) = (vx, vy + g)
+            (npx,npy) = (px + vx, py + vy)
+            g = -1
+            in Model {getVelocity = (nvx,nvy), getPostition = (npx,npy) , getObject = getObject model, getMass = getMass model } 
+            --                                                            shouldn't have to do this, but it crashed without it
         
         
