0b437337715e5f8231d7decabe2a1a30fbb91bf4
Jay Megill <jmegill7@unm.edu>
2024-11-20 16:30:04 -0700

    Added applyForces function, and the getForces function to Model. stateToState' now uses that instead to apply gravity. derived Show and Eq for Model

--- app/Force.hs
+++ app/Force.hs
@@ -2,29 +2,43 @@ module Force where
 
 import Graphics.Gloss.Data.Picture
 
-type Velocity = (Float, Float)
+
 -- this does work i was doing something wrong
 type State = [Model]
 
-data Model = Model {getVelocity :: Velocity, getPostition :: Point, getObject :: Object, getMass :: Float}
+data Model = 
+    Model {getVelocity :: (Float, Float), getPostition :: Point, getObject :: Object, getMass :: Float, getForces :: [(Float,Float)]}
+        deriving (Show, Eq)
 -- Sum type to hold the object in question currently just a box
 data Object = Box Float Float
+    deriving (Show, Eq)
+
+
+applyForces :: Model -> (Float, Float)
+applyForces m = 
+    let
+    mass      = getMass m
+    (vx,vy)   = getVelocity m
+    (nfx,nfy) = foldr (\(x1,y1) (x2,y2) -> (x1+x2,y1+y2) ) (0,0) (getForces m)
+    (ax,ay)   = (nfx/mass ,nfy/mass) 
+    in (vx + ax,vy + ay)
+
 
 -- Convert object withn the Model into a picture currently just the Box but more rendering could be possible
 objectToPicture :: Object -> Picture
 objectToPicture (Box w l) = Polygon [(w,l),(-w,l),(-w,-l),(w,-l)]
 
-initialVelocity :: Velocity
-initialVelocity = (0,0)
+initialVelocity :: (Float, Float)
+initialVelocity = (10,10)
 
 initialBox :: Model
-initialBox = Model initialVelocity (0,0) (Box 25 25) 50
+initialBox = Model initialVelocity (0,0) (Box 25 25) 50 [(0,-50)]
 
 --TODO: State functions are marked with ' as to not conflict with other files will need to be changed or merged later
 initalState' :: State
 initalState' = [initialBox,adjustedBox]
     where
-        adjustedBox = initialBox {getPostition = (100,-400) }
+        adjustedBox = initialBox {getPostition = (100,400) }
 
 stateToPicture' :: State -> Picture
 stateToPicture' state = Pictures (map toPicture state)
@@ -45,10 +59,9 @@ stateToState'  = map next
             let 
             (vx,vy) = getVelocity model
             (px,py) = getPostition model
-            (nvx,nvy) = (vx, vy + g)
+            (nvx,nvy) = applyForces model
             (npx,npy) = (px + vx, py + vy)
-            g = -1
-            in Model {getVelocity = (nvx,nvy), getPostition = (npx,npy) , getObject = getObject model, getMass = getMass model } 
-            --                                                          shouldn't have to do this, but it crashed without it
+            in model {getVelocity = (nvx,nvy), getPostition = (npx,npy) } 
+            -- Model == new decleration | model == remake old one with changes
         
         
