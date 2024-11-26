38f829ef2df2c17945a1707f530667f56079e6fa
xavier <vigenesisvi@gmail.com>
2024-11-26 16:58:07 -0700

    fixed moving the arm using arrow keys

--- app/Main.hs
+++ app/Main.hs
@@ -4,34 +4,34 @@ import Arm ( RobotArm(RobotArm), Link(Link), drawArm, updateArm )
 import Box (Box(Box), drawBox, constructBox, updateBoxPosition, boundaryCheck)
 import Graphics.Gloss.Interface.Pure.Game
 
-data Sim = Sim RobotArm Box Bool
+data Sim = Sim RobotArm Box Bool [SpecialKey]
 
 
 window :: Display
-window = InWindow "Window" (800, 800) (50, 50)
+window = InWindow "Window" (900, 800) (50, 50)
 
 initialArm :: RobotArm
-initialArm = RobotArm [Link 100 90, Link 100 0] (120, 106)
+initialArm = RobotArm [Link 100 78.27139, Link 100 (-73.63233)] (120, 106)
 
 initialBox :: Box
 initialBox = constructBox 30 50 120
 
 
 drawSim :: Sim -> Picture
-drawSim (Sim arm box _) = Pictures [drawArm arm, drawBox box, floorPic]
+drawSim (Sim arm box _ _) = Pictures [drawArm arm, drawBox box, floorPic]
   where
     -- base = translate 0 (-25) $ color (greyN 0.5) $ rectangleSolid 50 50
     floorPic = translate 0 (-25) $ color (greyN 0.5) $ rectangleSolid 800 50
 
 
 updateSim :: Float -> Sim -> Sim
-updateSim dt (Sim arm box isPushed) =
+updateSim dt (Sim arm box isPushed keys) =
     if not isPushed && boundaryCheck eePos boxPoints
-    then Sim newArm (updateBoxPosition pushDist eePos box) True  -- Push once and set flag
-    else Sim newArm box (boundaryCheck eePos boxPoints)  -- Reset flag when contact ends
+    then Sim newArm (updateBoxPosition pushDist eePos box) True keys -- Push once and set flag
+    else Sim newArm box (boundaryCheck eePos boxPoints) keys  -- Reset flag when contact ends
   where
     -- Update the arm and get the end-effector position
-    (newArm, eePos) = updateArm dt arm
+    (newArm, eePos) = updateArm dt (updateArmPositionsFromKeys arm keys)
 
     -- Extract box points for collision check
     Box _ boxPoints _ _ = box
@@ -39,29 +39,37 @@ updateSim dt (Sim arm box isPushed) =
     -- Fixed push distance
     pushDist = 5 -- Push distance
 
+updateArmPositionsFromKeys :: RobotArm -> [SpecialKey] -> RobotArm
+updateArmPositionsFromKeys (RobotArm links (x,  y)) keys = RobotArm links (newX, newY)
+  where
+    newX
+      | KeyRight `elem` keys = x+1
+      | KeyLeft `elem` keys = x-1
+      | otherwise = x
+
+    newY
+      | KeyUp `elem` keys = y+1
+      | KeyDown `elem` keys = y-1
+      | otherwise = y
+
+
 
 inputHandler :: Event -> Sim -> Sim
 inputHandler (EventKey (SpecialKey KeySpace) Down _ _) _ =
-    Sim initialArm initialBox False -- Reset simulation to initial state
-inputHandler 
-  (EventKey (SpecialKey KeyUp) Down _ _)
-  (Sim (RobotArm links (x, y)) box isPushed) =
-    Sim (RobotArm links (x, y+10)) box isPushed
+    Sim initialArm initialBox False [] -- Reset simulation to initial state
+
 inputHandler
-  (EventKey (SpecialKey KeyDown) Down _ _)
-  (Sim (RobotArm links (x, y)) box isPushed) =
-    if y-10 < 0
-    then Sim (RobotArm links (x, y)) box isPushed
-    else Sim (RobotArm links (x, y-10)) box isPushed
+  (EventKey (SpecialKey key) Down _ _)
+  (Sim arm box isPushed keys) =
+    Sim arm box isPushed (key : keys)
+
 inputHandler
-  (EventKey (SpecialKey KeyLeft) Down _ _)
-  (Sim (RobotArm links (x, y)) box isPushed) =
-    Sim (RobotArm links (x-10, y)) box isPushed
-inputHandler 
-  (EventKey (SpecialKey KeyRight) Down _ _)
-  (Sim (RobotArm links (x, y)) box isPushed) =
-    Sim (RobotArm links (x+10, y)) box isPushed
+  (EventKey (SpecialKey key) Up _ _)
+  (Sim arm box isPushed keys) =
+    Sim arm box isPushed (filter (/= key) keys)
+
 inputHandler event sim = sim
+
 
 
 main :: IO()
@@ -69,7 +77,7 @@ main = play
   window -- display
   (makeColorI 220 220 220 10)  -- window color
   60     -- number of simulation steps to take each second
-  (Sim initialArm initialBox False) -- initial state
+  (Sim initialArm initialBox False []) -- initial state
   drawSim      -- function to draw the objects in the simulation
   inputHandler -- input handler for user input
   updateSim    -- function to call on each simulation step
